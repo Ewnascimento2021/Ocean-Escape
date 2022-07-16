2c69d0b6090071025f8b46a7c07ab2312a448824
Ewnascimento <ewerton_snascimento@outlook.com>
2022-07-16 12:04:54 -0300

    Sereio and MousePositon

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,17 +4,24 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    private float speed;
+    [SerializeField]
+    private float speedH;
+    private Rigidbody2D rb;
+    private Vector2 mousePosition;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        mousePosition = Input.mousePosition;
+        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
         
+        rb.transform.position = new Vector2(mousePosition.x,rb.position.y);
+        Debug.Log(mousePosition.x);
     }
 }
