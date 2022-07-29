6c215204d3aea8626ff1a8643cd0322775f27baa
Ewnascimento <ewerton_snascimento@outlook.com>
2022-07-29 16:33:56 -0300

    Json - Track Creator

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public PlayerJson json;
+
+    private float initialTime;
+
+    private float elapsedTime;
+
+    void Start()
+    {
+        json = new PlayerJson();
+
+        json.Save();
+
+        //json.Load();
+
+        initialTime = Time.time;
+    }
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            elapsedTime = Time.time - initialTime;
+
+            json.AddNote (elapsedTime);
+
+            json.Save();
+
+           // json.Load();
+            
+        }
+    }
+  
+}
---
+++ Assets/Scripts/PlayerJson.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+[Serializable]
+public class PlayerJson
+{
+    public List<float> NotaTime = new List<float>();
+
+    public string path = "Assets/Player.txt";
+
+    public void Save()
+    {
+        var content = JsonUtility.ToJson(this, true);
+        File.WriteAllText(path, content);
+    }
+
+    //public void Load()
+    //{
+    //    var content = File.ReadAllText(path);
+    //    var p = JsonUtility.FromJson<PlayerJson>(content);
+    //    NotaTime = p.NotaTime;
+    //}
+    public void AddNote(float noteTime)
+    {
+        NotaTime.Add(noteTime);
+    }
+}
