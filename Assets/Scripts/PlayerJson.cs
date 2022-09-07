7ece141e1ca46ede5b1864c82d20d8c56974514a
Ewnascimento <ewerton_snascimento@outlook.com>
2022-09-07 13:43:37 -0300

    Atualização cod Json Level1

--- Assets/Scripts/PlayerJson.cs
+++ Assets/Scripts/PlayerJson.cs
@@ -11,7 +11,7 @@ public class PlayerJson
 
     public List<float> NotaTime = new List<float>();
 
-    public string path = "Assets/Player.txt";
+    public string path = "Assets/Fase1.txt";
 
     public void Save()
     {
